43b676c1dfeceb728f6a53be2065b79fd4609382
skin547 <skin547yo@gmail.com>
2024-08-19 02:43:48 +0800

    feat: implement api controller by applying counter service

--- api/src/main/java/com/taboola/api/ApiApp.java
+++ api/src/main/java/com/taboola/api/ApiApp.java
@@ -2,8 +2,10 @@ package com.taboola.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.taboola"})
 public class ApiApp {
 
     public static void main(String[] args) {
--- api/src/main/java/com/taboola/api/Controller.java
+++ api/src/main/java/com/taboola/api/Controller.java
@@ -1,18 +1,44 @@
 package com.taboola.api;
 
 import java.time.Instant;
+import java.util.List;
 
+import com.taboola.domain.Counter;
+import com.taboola.service.CounterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping("/api")
 public class Controller {
+    private final CounterService service;
+
+    @Autowired
+    public Controller(CounterService service) {
+        this.service = service;
+    }
+
 
     @GetMapping("/currentTime")
     public long time() {
         return Instant.now().toEpochMilli();
+    }
+
+
+    // 第一個 API: 根據 time bucket 獲取 counters
+    @GetMapping("/counters/time/{timeBucket}")
+    public List<Counter> getCountersByTimeBucket(@PathVariable("timeBucket") String timeBucket) {
+        return this.service.getCounters(timeBucket);
+    }
+
+    // 第二個 API: 根據 time bucket 和 event id 獲取特定 counter
+    @GetMapping("/counters/time/{timeBucket}/eventId/{eventId}")
+    public Counter getCounterByTimeBucketAndEventId(@PathVariable("timeBucket") String timeBucket,
+                                                    @PathVariable("eventId") String eventId) {
+        return this.service.getCounterByEventId(timeBucket, eventId);
     }
 
 }
